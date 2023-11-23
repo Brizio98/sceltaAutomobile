95600f07d9e7ed0df394161b8483435dfdbe4aef
Brizio98 <fabriziog.1998@gmail.com>
2023-11-23 12:21:38 +0100

    fine esercizio

---
+++ src/ogr/generation/italy/Automobili.java
@@ -0,0 +1,43 @@
+package ogr.generation.italy;
+
+public class Automobili {
+	
+	
+	int velocita;
+	float consumoMedio, carburante, totkmpercorsi;
+	
+	public void accelera() {
+		
+		if (velocita<=190)
+			velocita+=10;   // mi incrementa la velocita
+		
+		else 
+			System.out.println("velocita massima raggiunta");
+		}
+	public void decelera ()	{
+		if(velocita>=10)
+			velocita-=10;
+		else 
+			System.out.println("piu piano di cosi ti si ferma la macchina");
+	}
+		
+		
+	public void visualizzaDati () {
+			System.out.println("velocita attuale" + velocita);
+			System.out.println("carburante " + carburante + "l");
+			System.out.println("consumo medio: " + consumoMedio + "km/l");
+			System.out.println("kilometri percorsi " + totkmpercorsi);
+	}
+		
+	public void aggiorna() {
+		int kmpercorsi1h= velocita;
+		
+		totkmpercorsi += kmpercorsi1h;
+		carburante -= kmpercorsi1h/consumoMedio;
+	}
+		
+	
+	
+	
+	
+}
---
+++ src/ogr/generation/italy/Main.java
@@ -0,0 +1,72 @@
+package ogr.generation.italy;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc=new Scanner(System.in);
+		Automobili auto1 = new Automobili();
+		Automobili auto2 = new Automobili();
+		Automobili autoCorrente=auto1;
+		String risposta;
+		
+		auto1.velocita=0;
+		auto1.carburante=30;
+		auto1.consumoMedio=20;
+		auto1.totkmpercorsi=0;
+		
+		
+		auto2.velocita=0;
+		auto2.carburante=50;
+		auto2.consumoMedio=23;
+		auto2.totkmpercorsi=0;
+		
+		do {
+		
+		System.out.println("\n auto1: ");
+		auto1.visualizzaDati();
+		
+		System.out.println(" \n auto2: ");
+		auto2.visualizzaDati();
+		
+		
+		System.out.println("quale auto vuoi guidare? 1 o 2 ");
+		risposta=sc.nextLine();
+		
+		if (risposta.equals("1"))
+			autoCorrente=auto1;
+		else if (risposta.equals("2"))
+			autoCorrente=auto2;
+		
+		
+		
+		System.out.println("vuoi accelerare o decelerare?");
+		risposta=sc.nextLine();
+		
+		if (risposta.equals("accelerare"))
+			autoCorrente.accelera();
+		else if(risposta.equals("accelerare"))
+			autoCorrente.decelera();
+		
+		auto1.aggiorna();
+		auto2.aggiorna();
+	
+		System.out.println("vuoi continuare s/n");
+		risposta=sc.nextLine();
+		
+		
+		}while(risposta.equals("s"));
+		
+		
+	
+	
+	
+	
+	
+	
+	}
+
+}
